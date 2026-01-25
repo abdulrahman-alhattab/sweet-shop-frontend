2e41b9a92826835f52b9692ef5b7fdbd482ba7dd
abdulrahman-alhattab <osmiumh30@gmail.com>
2026-01-25 14:34:22 +0300

    setup front end

---
+++ src/components/Landing/Landing.jsx
@@ -0,0 +1,11 @@
+// This page is a simple component that just loads when the page first loads and you are not signed in
+const Landing = () => {
+  return (
+    <main>
+      <h1>Hello, welcome to the sweets shop</h1>
+      <p>Sign up now, or sign in to access the sweets</p>
+    </main>
+  )
+}
+
+export default Landing
